625e122d2d13299a12c2bf860ec5fd642e84178b
sheshmani09 <146670110+sheshmani09@users.noreply.github.com>
2025-07-16 11:02:28 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+// String: Text enclosed in quotes.
+// Number: Integers or floating-point numbers.
+// Boolean: True or false values.
+// Undefined: Variables declared but not assigned a value.
+// Null: Represents "no value."
+// Object: Non-primitive type for storing collections of data.
+// Array: A special type of object for storing lists of values.
+
+let Name = "john"
+console.log(typeof Name)
+
+let age = 28
+console.log(typeof age)
+
+let isAdult= true
+console.log(typeof isAdult)
+
+let kids= null
+console.log(typeof kids) //type of "null" is "object"
+
+let wife
+console.log(typeof wife)
+
+const user ={
+     Name : "john",
+     age : 28
+}
+console.log(typeof user)
