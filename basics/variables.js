7ac079d615ec89a88f150fe82982621313856c8b
sheshmani09 <146670110+sheshmani09@users.noreply.github.com>
2025-07-16 10:28:13 +0000

    leanred about variables

---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 1234  //constant value - means value cannot be changed , if you change it will throw error
+accountId = 2
+let accountPassword= "23245"
+var accountCity= "Jaipur"
+
+// never use var - it has scope problem , i.e. - if you change one variable named password it will change the value of all variables named password irrespective of block or scope
