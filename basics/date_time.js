77664558436b5218878ff196f0578f4feb1e4001
sheshmani09 <146670110+sheshmani09@users.noreply.github.com>
2025-07-18 12:29:06 +0000

    learned about date and time

---
+++ basics/date_time.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString()) //return the whole value as string 
+// e.g Fri Jul 18 2025 11:54:21 GMT+0000 (Coordinated Universal Time)
+
+console.log(myDate.toDateString()) //returns date string only no time ex- Fri Jul 18 2025
+console.log(myDate.toLocaleDateString()) //returns date only wihtout day ex-7/18/2025
+console.log(myDate.toLocaleString()) //return date and time in this format-  7/18/2025, 11:57:25 AM
+console.log(myDate.toISOString()) //return in iso format
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleTimeString()) //gives only time
+console.log(myDate.toTimeString()) //utc format time
+console.log(myDate.toUTCString()) //day date  and time 
+
+// let createDate = new Date(2025,8,21)
+let createDate = new Date("sept 21, 2025")
+console.log(createDate.toLocaleDateString("hi-IN"));//to convert to indian format
