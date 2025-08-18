6e41ef456dfd7a54df8eb18a6ea92ab5ba753db3
sheshmani09 <146670110+sheshmani09@users.noreply.github.com>
2025-08-18 17:45:32 +0000

    objects part 2

---
+++ arrays/objects2.js
@@ -0,0 +1,18 @@
+
+const obj1 ={ 1: 'a', 2: 'b' };
+const obj2 = { 3: 'c', 4: 'd' };
+const obj3 = { 5: 'e', 6: 'f' };
+
+// const obj4 = Object.assign(obj1, obj2, obj3); this treats obj1 as target and modifies it
+const obj4 = Object.assign({}, obj1, obj2, obj3); //this is better way to merge objects without modifying the original ones
+
+const obj5 = {...obj1, ...obj2, ...obj3}; //best way to merge objects using spread operator
+// console.log(obj5);
+
+console.log(Object.keys(obj5)); //returns keys of the object in array format
+console.log(Object.values(obj5)); //returns values of the object in array format
+console.log(Object.entries(obj5)); //returns key-value pairs of the object in array format
+
+console.log(obj5.hasOwnProperty('5')); //returns true if the object has the property '5'
+
+
