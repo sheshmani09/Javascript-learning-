1abdf2ead04aafb73ba7b98459e49bfdee44715e
sheshmani09 <146670110+sheshmani09@users.noreply.github.com>
2025-08-19 12:15:17 +0000

    functions

--- basics_3/functions01.js
+++ basics_3/functions01.js
@@ -26,5 +26,40 @@ function loginUserMsg(user = "Sam"){   //here we gave default value to user in c
     return `${user} logged in`
 }
 
-console.log(loginUserMsg()); //this will print default value
-console.log(loginUserMsg("Rahul"));
+// console.log(loginUserMsg()); //this will print default value
+// console.log(loginUserMsg("Rahul"));
+
+// function calculateCartTotal(num1){
+//     return num1
+// }
+// console.log(calculateCartTotal(200,300,500)); returns only first value
+
+function calculateCartTotal(...num){  //rest operator (spread operator)
+    return num
+}
+
+// console.log(calculateCartTotal(200,300,500));  //[ 200, 300, 500 ] output
+
+const product = {
+    productName : "Shampoo",
+    price: 999
+}
+
+function handleObject(anyobject) {  
+    console.log(`product name is ${anyobject.productName} and price is Rs. ${anyobject.price}`);
+}
+handleObject(product) //one way to pass object in argument
+
+handleObject({
+    productName: "soap",
+    price: 299
+})
+
+//similarly we can pass array as well
+
+let myArray = [200, 100, 400]
+
+function handleArray(getArray){
+    console.log(getArray[2]); 
+}
+handleArray(myArray)
