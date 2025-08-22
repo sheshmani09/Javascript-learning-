8c4bb6a6735c325a0a5636842beb7b43071dbb14
sheshmani09 <146670110+sheshmani09@users.noreply.github.com>
2025-08-22 17:51:20 +0000

    reduce

--- basics_3/array-loops2.js
+++ basics_3/array-loops2.js
@@ -22,7 +22,7 @@ nums.forEach((num)=>{
 // console.log(newnums);
 
 
-//map function
+//map function  -this is a function
 const mynumerics = [1,2,3,4,5,6,7,8,9]
 
 const newnumerics = mynumerics.map((num)=> num+10 )
@@ -37,4 +37,35 @@ const newnum = mynumerics.map((num)=> num*10).map((num)=> num+1).filter((num)=> 
 
 // console.log(newnum);
 
+
+
+//reduce function
+
+const arr1 = [1,2,3,4,5]
+
+let sum  = arr1.reduce((acc,curr)=> acc+curr, 0)
+
+// let sum = arr1.reduce((acc,curr)=>{
+//     console.log(`accumulator = ${acc} and current value = ${curr}`);
+    
+//         return acc+curr
+// },0)
+// let sum = arr1.reduce((acc,curr)=>{
+//     console.log(`accumulator = ${acc} and current value = ${curr}`);
+    
+//         return acc+curr
+// }) if you dont give initial value it will start from 1st value of array
+
+console.log(sum);
+
+
+const shopcart = [ 
+    { name: "js course", price : 999},
+    { name: "python course", price : 1299},
+    { name: "cpp course", price : 799}
+]
+
+const price = shopcart.reduce((acc,item)=> (acc + item.price),0)
+
+console.log(price);
 
