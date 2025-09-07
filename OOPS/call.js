6bd853edff432ea1af786e22dafaec0eb7461fa7
sheshmani09 <sheshmanisahu09@gmail.com>
2025-09-07 18:11:31 +0530

    explicit call in js

---
+++ OOPS/call.js
@@ -0,0 +1,19 @@
+function setUsername(username){
+    //DB calls
+    this.username = username
+    console.log("called");
+    
+}
+
+function createUser(username, email, pass){
+    // setUsername(username)        //this wont run, we need to call it explicitly to give them context
+    // setUsername.call( username) //called  but still not returning value
+// createUser { email: 'abc@googgle.com', pass: 123 }
+
+setUsername.call(this,username)
+    this.email = email
+    this.pass = pass
+}
+
+const chai = new createUser("rahul","abc@googgle.com",123)
+console.log(chai);
