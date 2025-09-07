37fcd540238f8f464508c3bf7177841420620227
sheshmani09 <sheshmanisahu09@gmail.com>
2025-09-07 23:43:12 +0530

    separated static keyword

--- OOPS/inheritance.js
+++ OOPS/inheritance.js
@@ -41,30 +41,5 @@ supra.start();
 
 
 
-// static keyword 
-
-class Calculator {
-  static add(a, b) {
-    return a + b;
-  }
-}
-
-// console.log(new Calculator().add(5,10))  -it will give error
-console.log(Calculator.add(5, 10)); // 15
-
-//another example
-class User {
-  static count = 0; // shared across all users
-
-  constructor(name) {
-    this.name = name;
-    User.count++; // increment every time a user is created
-  }
-}
-
-let u1 = new User("Rahul");
-let u2 = new User("Amit");
-
-console.log(User.count); 
-console.log(u2.name); 
+ 
 
